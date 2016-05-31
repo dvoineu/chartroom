4e91c77043dcad4c4c9f63828fb6901a84ec9b74
advoineu <Aliaksandr.Dvoineu@cocomore.com>
2016-05-31 11:33:34 +0200

    New changes

--- server.js
+++ server.js
@@ -18,28 +18,28 @@ function sendFile (response, filePath, fileContents) {
 }
 
 function serveStatic(response, cache, absPath) {
-// Проверка факта кэширования файла в памяти
-if (cache[absPath]) {
-// Обслуживание файла, находящегося в памяти
-sendFile(response, absPath, cache[absPath]);
-} else {
-// Проверка факта существования файла
-fs.exists(absPath, function(exists) {
-if (exists) {
-// Считывание файла с диска
-fs.readFile(absPath, function(err, data) {
-if (err) {
-send404(response);} else {
-cache[absPath] = data;
-// Обслуживание файла, считанного с диска
-sendFile(response, absPath, data);
-}
+	// Проверка факта кэширования файла в памяти
+	if (cache[absPath]) {
+		// Обслуживание файла, находящегося в памяти
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		// Проверка факта существования файла
+		fs.exists(absPath, function(exists) {
+	if (exists) {
+		// Считывание файла с диска
+		fs.readFile(absPath, function(err, data) {
+	if (err) {
+		send404(response);} else {
+		cache[absPath] = data;
+		// Обслуживание файла, считанного с диска
+		sendFile(response, absPath, data);
+	}
 });
-} else {
-// Отсылка HTTP-ответа 404
-send404(response);
-}
-});
+	} else {
+		// Отсылка HTTP-ответа 404
+		send404(response);
+		}
+	});
 }
 }
 
@@ -57,3 +57,6 @@ var server = http.createServer(function(request, response){
 server.listen(3000, function(){
 	console.log('Server listening on the port 3000');
 });
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
